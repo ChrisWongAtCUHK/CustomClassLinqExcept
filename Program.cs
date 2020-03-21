93ccc24e0a80006236999f5eda969b49f93d34f3
Chris Wong <wc38.wong@gmail.com>
2020-03-21 09:02:58 +0800

    IEquatable<T> implementation

--- Program.cs
+++ Program.cs
@@ -9,34 +9,9 @@ namespace CustomClassLinqExcept
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("\n\n==========Class except==========");
-      Test1();
-      Console.WriteLine("\n\n==========Different instance==========");
+      Console.WriteLine("\n\n==========IEquatable<T> implementation==========");
       Test2();
-      Console.WriteLine("\n\n==========Custom comparer==========");
-      Test3();
     }
-
-    static void Test1()
-    {
-      var original = new List<User>()
-    {
-        new User{ Id = "A01", Name = "Jeffrey" },
-        new User{ Id = "A02", Name = "Darkthread" }
-    };
-
-      var merged = new List<User>();
-      merged.AddRange(original);
-      merged.Add(new User
-      {
-        Id = "B01",
-        Name = "Ironman"
-      });
-
-      var diff = merged.Except(original);
-      Console.WriteLine(JsonConvert.SerializeObject(diff));
-    }
-
     static void Test2()
     {
       var original = new List<User>()
@@ -63,34 +38,6 @@ namespace CustomClassLinqExcept
 
       var diff = fromReq.Except(fromDb);
       Console.WriteLine(JsonConvert.SerializeObject(diff, Formatting.Indented));
-    }
-
-    static void Test3()
-    {
-      var original = new List<User>()
-        {
-            new User{ Id = "A01", Name = "Jeffrey" },
-            new User{ Id = "A02", Name = "Darkthread" }
-        };
-      var originalJson = JsonConvert.SerializeObject(original);
-
-      var merged = new List<User>();
-      merged.AddRange(original);
-      merged.Add(new User
-      {
-        Id = "B01",
-        Name = "Ironman"
-      });
-      var mergedJson = JsonConvert.SerializeObject(merged);
-
-      //模擬情境
-      //original 由資料庫欄位 JSON 還原
-      //merged 由 WebAPI Request 傳入 JSON 還原
-      var fromDb = JsonConvert.DeserializeObject<List<User>>(originalJson);
-      var fromReq = JsonConvert.DeserializeObject<List<User>>(mergedJson);
-
-      var diff = fromReq.Except(fromDb, new UserComparer());
-      Console.WriteLine(JsonConvert.SerializeObject(diff));
     }
   }
 }
--- User.cs
+++ User.cs
@@ -1,8 +1,20 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+
 namespace CustomClassLinqExcept
 {
-  public class User
+  public class User : IEquatable<User>
   {
     public string Id { get; set; }
     public string Name { get; set; }
+
+    public bool Equals(User other)
+    {
+      if (other == null) return false;
+      return this.Id == other.Id;
+    }
+
+    public override bool Equals(object obj) => Equals(obj as User);
+    public override int GetHashCode() => Id.GetHashCode();
   }
 }
