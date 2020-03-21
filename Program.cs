5cb8c9c5ebf316cba8adc854eda4eb53f81d3fad
Chris Wong <wc38.wong@gmail.com>
2020-03-21 08:52:15 +0800

    User 為不同的 Instance

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace CustomClassLinqExcept
     static void Main(string[] args)
     {
       Test1();
+      Test2();
     }
 
     static void Test1()
@@ -28,9 +29,36 @@ namespace CustomClassLinqExcept
         Name = "Ironman"
       });
 
-
       var diff = merged.Except(original);
       Console.WriteLine(JsonConvert.SerializeObject(diff));
+    }
+
+    static void Test2()
+    {
+      var original = new List<User>()
+      {
+          new User{ Id = "A01", Name = "Jeffrey" },
+          new User{ Id = "A02", Name = "Darkthread" }
+      };
+      var originalJson = JsonConvert.SerializeObject(original);
+
+      var merged = new List<User>();
+      merged.AddRange(original);
+      merged.Add(new User
+      {
+        Id = "B01",
+        Name = "Ironman"
+      });
+      var mergedJson = JsonConvert.SerializeObject(merged);
+
+      //模擬情境
+      //original 由資料庫欄位 JSON 還原
+      //merged 由 WebAPI Request 傳入 JSON 還原
+      var fromDb = JsonConvert.DeserializeObject<List<User>>(originalJson);
+      var fromReq = JsonConvert.DeserializeObject<List<User>>(mergedJson);
+
+      var diff = fromReq.Except(fromDb);
+      Console.WriteLine(JsonConvert.SerializeObject(diff, Formatting.Indented));
     }
   }
 }
