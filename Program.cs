bce09294e7d965116d3068c6f9ff65cc30c56045
Chris Wong <wc38.wong@gmail.com>
2020-03-21 08:47:20 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace CustomClassLinqExcept
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Test1();
+    }
+
+    static void Test1()
+    {
+      var original = new List<User>()
+    {
+        new User{ Id = "A01", Name = "Jeffrey" },
+        new User{ Id = "A02", Name = "Darkthread" }
+    };
+
+      var merged = new List<User>();
+      merged.AddRange(original);
+      merged.Add(new User
+      {
+        Id = "B01",
+        Name = "Ironman"
+      });
+
+
+      var diff = merged.Except(original);
+      Console.WriteLine(JsonConvert.SerializeObject(diff));
+    }
+  }
+}
